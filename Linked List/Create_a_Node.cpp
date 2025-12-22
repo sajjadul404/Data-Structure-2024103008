3ba70e6235e3f027885e02b79b7a013ab5b7869e
sajjadul404 <sajjadul625@gmail.com>
2025-12-22 22:37:49 +0600

    create a node

--- Linked List/Create_a_Node.cpp
+++ Linked List/Create_a_Node.cpp
@@ -1,26 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-class Node{
-    public:
-    int val;
-    Node* next;
-
-    Node(int val){
-        this->val = val;
-        this->next = NULL;
-    }
+struct node{
+    int data;
+    node* left;
+    node*right;
 };
 
-int main(){
-    Node a(10), b(20), c(300);
+node *creat(){
+    int x;
+    cin>>x;
 
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
+    node* newnode= new node;
 
-    cout << a.val << endl;
-    cout << a.next->val << endl;
-    return 0;
+    if(x==-1){
+        return 0;
+    }
 
+    newnode->data=x;
+    cout<<"Left child of"<<x<<" :";
+     newnode->left=creat();
+
+    cout<<"Right child of"<<x<<" :";
+    newnode->right=creat();
+
+    return newnode;
 }
+ void inorder(node *root){
+    if(root==NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+ }
+  void preorder(node *root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+ }
+  void postorder(node *root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+ }
+ int main(){
+     cout<<"Enter Root node: ";
+    node *root;
+    root=creat();
+    preorder(root);
+    cout<<endl;
+    inorder(root);
+    cout<<endl;
+    postorder(root);
+    cout<<endl;
+ }
